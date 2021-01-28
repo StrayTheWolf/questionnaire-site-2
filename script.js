29a7da46fd657eea5237c453929f1a7e4f9da0ba
Wolf Stanislav <ork2004@yandex.ru>
2021-01-28 22:50:53 +0300

    working on search in <input> logic

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-document.addEventListener('DOMContentLoaded',()=>{
-   document.getElementById('submitInfo').addEventListener('click', ()=>{
-        console.log('click');
-           let input = document.getElementById('fullName');
-           console.log(input);
-            console.log(typeof input.innerHTML);
-            let name = input.innerHTML;
-            console.log(name.length);
-           })
-   })
+function getInfo(id){
+    return document.getElementById(id).value;
+}
+
+
+let array = [];
+function addArray(){
+    let data = document.querySelectorAll('.search')
+    for (let i = 0; i < data.length; i++){
+        array[i] = data[i].value;
+    }
+    console.log(array);
+};
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('submitInfo').addEventListener('click', () => {
+
+        let name = getInfo('fullName')
+        console.log(name);
+
+
+        (function() {
+            let inputs = document.querySelectorAll('.search');
+            let arrayData = [];
+
+            function save() {
+                for (let i = 0; i < inputs.length; i++) {
+                    arrayData[i] = inputs[i].value;
+                }
+                console.log(arrayData);
+            }
+            save();
+        })();
+
+    })
+
+})
