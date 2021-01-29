b7048502353f8172edfd25f3cbe8e6c72c19ed82
Wolf Stanislav <ork2004@yandex.ru>
2021-01-29 18:49:07 +0300

    optimizing spaces in code

--- script.js
+++ script.js
@@ -4,41 +4,45 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 })
 
-function openBlock2(){
+function openBlock2() {
     document.getElementById('block1').style.display = 'none';
     document.getElementById('block2').style.display = 'block';
 }
-function openBlock3(){
+
+function openBlock3() {
     document.getElementById('block2').style.display = 'none';
     document.getElementById('block3').style.display = 'block';
 }
-function openBlock4(){
+
+function openBlock4() {
     document.getElementById('block3').style.display = 'none';
     document.getElementById('block4').style.display = 'block';
 }
-function openBlock5(){
+
+function openBlock5() {
     document.getElementById('block4').style.display = 'none';
     document.getElementById('block5').style.display = 'block';
 }
 
 
 let array = [];
-function addArray(){
+
+function addArray() {
     let data = document.querySelectorAll('.search')
-    for (let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         array[i] = data[i].value;
     }
     console.log(array);
 }
 
-function showSummary(){
-    for (let i = 0; i < array.length; i++){
+function showSummary() {
+    for (let i = 0; i < array.length; i++) {
         let html = array[i];
         document.getElementById('summary').insertAdjacentHTML("beforeend", ' ' + html + ' ');
     }
 }
 
-function getInfo(id){
+function getInfo(id) {
     return document.getElementById(id).value;
 }
 
